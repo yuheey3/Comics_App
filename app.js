1649aa1ce6d1459c49d09a5888bff253b4ea0fc9
unknown <yuheey3@yahoo.co.jp>
2021-06-13 02:03:36 -0400

    connectAPI

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require("express");
 const exphbs = require(`express-handlebars`);
 var path = require("path");
+const bodyParser = require('body-parser');
+const request = require("request")
+const dotenv = require("dotenv").config()
 
 const homeRoutes = require("./routes/Home");
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use("/", homeRoutes);
 
@@ -13,9 +17,37 @@ app.use("/", homeRoutes);
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+
+//const address = process.argv[2]
+const url = `https://xkcd.com/info.0.json`
+//const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${process.env.API_KEY}`
+
+// if (!address) {
+//   return console.log("Please enter the name of the city")
+// }
+var month = "";
+request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+
+    app.month = data.month;
+    app.num = data.num;
+    app.year = data.year;
+    app.alt = data.alt;
+    app.img = data.img;
+    app.title = data.title;
+    app.day = data.day
+})
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Your web server is connected at ${PORT}`);
 
 });
+
+module.exports = app;
+
+
+
+
--- routes/Home.js
+++ routes/Home.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const router = express.Router();
+var app = require("../app.js");
+var s = "s";
 
+const url = `https://xkcd.com/info.0.json`
 
 router.get("/", (req, res) => {
 
-    res.render("Home/home");
+    res.render("Home/home", {
+        title: req.app.title, img: req.app.img, month: req.app.month,
+        num: req.app.num, year: req.app.year
+        , alt: req.app.alt, day: req.app.day
+    });
+
+
 
 });
 
-module.exports=router;
+module.exports = router;
+
